0b8e21b7dd762011ef5c729b3a7bdfb1a582625a
KwameSei <nathanielosei92@gmail.com>
2022-11-16 16:35:50 +0000

    Traversing nodes in inorder

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Traversing in inorder
+ * @tree: pointer to the
+ * @func: pointer to a function
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if ((tree == NULL) || (func == NULL))
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
